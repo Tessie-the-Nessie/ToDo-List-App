9b5c06833247abeec3334961b154b9dc7c531b0f
Tessa Delsener <wildtessa.70@icloud.com>
2024-05-05 11:11:37 -0700

    First iteration with both views

--- ToDo List App/ContentView.swift
+++ ToDo List App/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                
+                Text("ToDo List!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                
+                Spacer()
+                
+                Button {
+                    
+                    withAnimation {
+                        
+                        self.showNewTask = true
+                    }
+                    
+                } label: {
+                    Text("+")
+                }
+                .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.circle/*@END_MENU_TOKEN@*/)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                
+            }
+            
+            Spacer()
         }
-        .padding()
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        
+        if showNewTask {
+            NewToDoView()
+        }
+        
     }
 }
 
--- ToDo List App/NewToDoView.swift
+++ ToDo List App/NewToDoView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct NewToDoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Task Title:")
+            
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
+            
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }
+            
+            Button {
+
+            } label: {
+                Text("Save")
+                    .font(.callout)
+                    .foregroundColor(Color.red)
+            }
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
