edc9c2a4ae9b1a8b22270e3ae9c31e44b02acfa5
Tessa Delsener <wildtessa.70@icloud.com>
2024-05-05 10:19:02 -0700

    Initial Commit

---
+++ ToDo List App/ToDo_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_List_AppApp.swift
+//  ToDo List App
+//
+//  Created by Tessa Delsener on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDo List AppUITests/ToDo_List_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ToDo_List_AppUITestsLaunchTests.swift
+//  ToDo List AppUITests
+//
+//  Created by Tessa Delsener on 5/5/24.
+//
+
+import XCTest
+
+final class ToDo_List_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
